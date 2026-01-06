9367291dc65690e5d59964d21261b39f68103d6b
Prince Modi <krishnahzrbgh0615@gmail.com>
2026-01-06 19:41:53 +0530

    Add files via upload

---
+++ video 54/script.js
@@ -0,0 +1,20 @@
+alert("Hello World")
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want leave this page and blast your computer")
+
+if (isTrue) {
+    console.log("Your computer is blasting")
+}
+else {
+    console.log("Your computer is not blasting")
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey i am good"
+// document.body.style.backgroundColor = "blue"
