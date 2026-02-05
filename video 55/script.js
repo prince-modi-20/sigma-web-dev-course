6035c3c6dd98f1480c9089e61d74e4b3a487904c
Prince Modi <krishnahzrbgh0615@gmail.com>
2026-02-05 23:43:25 +0530

    Add files via upload

---
+++ video 55/script.js
@@ -0,0 +1,40 @@
+console.log("Hey this is tutorial 55");
+
+let a = 5;
+// a = a + 1
+let b = 6;
+let c = "Prince";
+// var 55a = "Rohan"; // Not Allowed
+
+// console.log(a + b + 9)
+// console.log(typeof a, typeof b, typeof c)
+
+{
+  let a = 66;
+  console.log(a);
+}
+console.log(a);
+// const a1 = 6;
+// a1 = a1 + 1; // Not Allowed because a1 is constant
+
+let x = "Prince bhai";
+let y = 22;
+let z = 2.5;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+  name: "Prince",
+  "job code": 5000,
+  is_handsome: true,
+  is_weird: false,
+};
+console.log(o);
+o.salary = "1000crores";
+console.log(o);
+o.salary = "2000crores";
+console.log(o);
