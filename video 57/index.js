32491c69ae3d858a81fa8a17462eb4e04445063e
Prince Modi <krishnahzrbgh0615@gmail.com>
2026-02-06 16:55:14 +0530

    Add files via upload

---
+++ video 57/index.js
@@ -0,0 +1,36 @@
+console.log("I am tutorial on loops");
+
+// let a = 1;
+// console.log(a);
+// console.log(a + 1);
+// console.log(a + 2);
+
+// for (let i = 0; i < 100; i++) {
+//   console.log(a + i);
+// }
+
+// let obj = {
+//   name: "Prince",
+//   role: "Programmmer",
+//   company: "Code by Prince AI",
+// };
+
+// for (const key in obj) {
+//   console.log(key);
+// }
+
+// for (const c of "Prince") {
+//   console.log(c);
+// }
+
+// let i = 0;
+// while (i < 6000) {
+//   console.log(i);
+//   i++;
+// }
+
+let i = 10;
+do {
+  console.log(i);
+  i++;
+} while (i<6);
