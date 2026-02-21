51b5edba1b526736d5e66e44a1705b3f9a9bcfec
Prince Modi <krishnahzrbgh0615@gmail.com>
2026-02-21 17:29:26 +0530

    Add files via upload

---
+++ video 60/index.js
@@ -0,0 +1,25 @@
+console.log("This is string tutorial");
+let a = "Prince";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+// console.log(a[6]);
+console.log(a.length);
+
+let Real_name = "Prince";
+let friend = "Rishi";
+console.log("His name is " + Real_name + " and his friend name is " + friend);
+console.log(`His name is ${Real_name} and his friend name is ${friend}`);
+
+let b = "Kishan";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1, 5));
+console.log(b.slice(1));
+
+console.log(b.replace("Kishan", "Anu"));
+console.log(b.concat(a, "Golu", "Anu", "Rishi"));
